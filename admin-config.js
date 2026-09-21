32343148d621b8b3633aae750ece69361d042b5d
samuRAIzy <rizza8autista@gmail.com>
2026-09-21 19:07:19 +1200

    Add files via upload

---
+++ admin-config.js
@@ -0,0 +1,4 @@
+// Paste the URL of the SEPARATE, owner-only Google Apps Script deployment.
+window.ADMIN_CONFIG = {
+  DASHBOARD_URL: "PASTE-PRIVATE-ADMIN-WEB-APP-URL-HERE"
+};
---
+++ config.js
@@ -0,0 +1,3 @@
+window.APP_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbxKzBhUaTzSxf5VdtZbPuO8I9q2Qzz00vGQgXsx2iNKNdkpzIYbKlHvPRoaKXqDUR-JrQ/exec"
+};
